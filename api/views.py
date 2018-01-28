3ea8bfb0f5d05b05f052da918e455d45d8183f3e
zackmcginnis <zm3@pdx.edu>
2018-01-27 21:38:30 -0800

    built api endpoint

--- api/views.py
+++ api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
+from django.http import JsonResponse
 
 # Create your views here.
 
 def index(request):
-    print(request)
-    return HttpResponse("Hello, world. You're at the polls index.")
+    print("testing this request")
+    query = request.GET.urlencode()
+    cleanedQuery = query[6:]
+    # GET TWEEPY RESULTS FOR QUERY
+    return JsonResponse({"name": 'test reponse ***2342343****', "queryTest": cleanedQuery}, safe=False)
